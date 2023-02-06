0e5a6d5174d241300e58b8310ee26465ae24132c
henigesa <121961371+henigesa@users.noreply.github.com>
2023-02-05 22:17:29 -0500

    including code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+//import scanner
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        //declare variables
+        int springCost ;
+        int summerCost ;
+        int fallCost ;
+        int winterCost ;
+        double totalCost ;
+
+        //prompt user to enter the cost
+        System.out.println("Please enter the price of your spring maintenance cost:");
+        //take the user input
+        springCost = input.nextInt() ;
+        //prompt user to enter the cost
+        System.out.println("Please enter the price of your summer maintenance cost:");
+        //take the user input
+        summerCost = input.nextInt() ;
+        System.out.println("Please enter the price of your fall maintenance cost:");
+        //take the user input
+        fallCost = input.nextInt() ;
+        System.out.println("Please enter the price of your winter maintenance cost:");
+        //take the user input
+        winterCost = input.nextInt() ;
+        //calculate total cost
+        totalCost = springCost + summerCost + fallCost + winterCost ;
+        //output the calculated total cost
+        System.out.println("Your total home maintenance cost for the year is" + totalCost) ;
     }
 }
